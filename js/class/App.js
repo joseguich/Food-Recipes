f6927e310da2ee09befd4d08fd964d25b7fa23b1
joseguich <joseguichardomieses@gmail.com>
2023-07-16 13:42:28 -0400

    Runs the entire program

--- js/class/App.js
+++ js/class/App.js
@@ -1,9 +1,28 @@
+import { $containerFavority, $selectCategory } from "../const.js";
+import { obtenerCategoria, seleccionarCategory } from "../api/fetch.js";
+import { getFavority } from "../func/show-favority.js";
+
 export default class App {
-   constructor() {
-      this.init();
-   }
+  constructor() {
+    this.init();
+  }
 
-   init() {
+  init() {
+    window.addEventListener("load", () => {
 
-   }
+      //Validar que existe 
+      if ($selectCategory) {
+        $selectCategory.addEventListener("change", seleccionarCategory);
+        obtenerCategoria();
+      }
+
+      //Validar existencia de favorito
+      if ($containerFavority) {
+        getFavority();
+      }
+
+
+
+    });
+  }
 }
